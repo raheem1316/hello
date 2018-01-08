5da7bb06b82cde76a2ef4285ce03e3c556de90ef
raheem1316 <34540315+raheem1316@users.noreply.github.com>
2018-01-08 16:10:20 +0530

    Update settings.py

--- deploy/settings.py
+++ deploy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i#%3(acuaqcttg65qhykbgm#pwl-7!^v%ofdbkyy$3-!muts)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
